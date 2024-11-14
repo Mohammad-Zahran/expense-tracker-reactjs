cc19fee98a4efb6c1e012c81d338d80edd33f2ce
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-11-14 20:53:26 +0200

    added the html elements to render the website

--- src/components/TransactionForm.js
+++ src/components/TransactionForm.js
@@ -15,8 +15,42 @@ const TransactionForm = ({addTransaction}) => {
         addTransaction({ ...formData, amount: parseFloat(formData.amount), id: Date.now() });
         setFormData({ name: '', amount: 0, date: '', type: 'expense' });
       };
-    
 
-}
+      return (
+        <form onSubmit={handleSubmit}>
+          {/* Transaction Type */}
+          <div>
+            <label>
+              <input
+                type="checkbox"
+                checked={formData.type === 'income'}
+                onChange={() => setFormData({ ...formData, type: formData.type === 'income' ? 'expense' : 'income' })}
+              />
+              <div className="option">
+                <span>Expense</span>
+                <span>Income</span>
+              </div>
+            </label>
+          </div>
+    
+          {/* Name, Amount, Date */}
+          <div>
+            <label>Note</label>
+            <input type="text" required value={formData.name} onChange={(e) => setFormData({ ...formData, name: e.target.value })} />
+          </div>
+          <div>
+            <label>Amount</label>
+            <input type="number" required min="0.01" step="0.01" value={formData.amount} onChange={(e) => setFormData({ ...formData, amount: e.target.value })} />
+          </div>
+          <div>
+            <label>Date</label>
+            <input type="date" required value={formData.date} onChange={(e) => setFormData({ ...formData, date: e.target.value })} />
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      );
+    
+      // In generel This component is used to handle the input the user will add. 
+};
 
 export default TransactionForm
