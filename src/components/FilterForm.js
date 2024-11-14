5bed7cc007c63f698232dfcb672893ed45b54426
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-11-14 22:05:31 +0200

    Added the return of the FilterForm

--- src/components/FilterForm.js
+++ src/components/FilterForm.js
@@ -23,6 +23,23 @@ const FilterForm = () => {
           })
         );
       };
+      
+      return (
+        <form onSubmit={applyFilter}>
+          <input type="number" placeholder="Min Amount" onChange={(e) => setFilters({ ...filters, minAmount: e.target.value })} />
+          <input type="number" placeholder="Max Amount" onChange={(e) => setFilters({ ...filters, maxAmount: e.target.value })} />
+          <select onChange={(e) => setFilters({ ...filters, type: e.target.value })}>
+            <option value="">All</option>
+            <option value="income">Income</option>
+            <option value="expense">Expense</option>
+          </select>
+          <input type="date" onChange={(e) => setFilters({ ...filters, date: e.target.value })} />
+          <input type="text" placeholder="Note" onChange={(e) => setFilters({ ...filters, note: e.target.value })} />
+          <button type="submit">Filter</button>
+        </form>
+      );
     }
+
+    
 
 export default FilterForm
