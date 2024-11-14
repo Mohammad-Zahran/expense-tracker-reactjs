eb40c77eb0ad53695a3c2a4ecb47c0ace2b66bfe
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-11-14 21:00:15 +0200

    imported BudgetSummary and rendering it as jsx tags

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import BudgetSummary from './components/BudgetSummary';
+import TransactionForm from './components/TransactionForm';
 
 function App() {
+  const [transactions, setTransactions] = useState(JSON.parse(localStorage.getItem("transactions")) || []);
+
+  useEffect(() => {
+    localStorage.setItem("transactions", JSON.stringify(transactions));
+  }, [transactions]);
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Expense Tracker</h1>
+      <BudgetSummary transactions={transactions} />
     </div>
-  );
+  )
 }
 
 export default App;
